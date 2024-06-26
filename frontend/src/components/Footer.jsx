032fb9601c11da1fbf0e5960c11473bc804518c7
ZsigriPeter <145133141+ZsigriPeter@users.noreply.github.com>
2024-06-26 11:25:13 +0200

    MainPage viewable (Outlet)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+
+function Footer() {
+
+    return (
+        <div className="footer">
+            <footer className="footer">
+                <div className="contacts">
+                    <span>Contact us: </span>
+                    <span>contact@mail.com</span>
+                    <br/>
+                    <span>+36 20 5556677</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,28 @@
-
+import {Outlet} from 'react-router-dom';
 
 function NavBar() {
 
     return (
-        <nav>
+        <>
             <div className="nav-bar">
-            <img src="" className="logo-img" alt="logo of the webpage"></img>
-            <ul className="nav-links">
-                <li><a href="/">Weekly menu</a></li>
-                <li><a href="/">Food list</a></li>
-                <li><a href="/">FAQ</a></li>
-                <li><a href="/">Orders</a></li>
-                <li><a href="/">Favorites</a></li>
-                <li><a href="/">User profile</a></li>
-                <li><a href="/">Log In</a></li>
-            </ul>
-        </div>
-        </nav>
-        
+                <nav>
+                    <img src="" className="logo-img" alt="logo of the webpage"></img>
+                    <ul className="nav-links">
+                        <li><a href="/main">Weekly menu</a></li>
+                        <li><a href="/">Food list</a></li>
+                        <li><a href="/">FAQ</a></li>
+                        <li><a href="/">Orders</a></li>
+                        <li><a href="/">Favorites</a></li>
+                        <li><a href="/">User profile</a></li>
+                        <li><a href="/">Log In</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="content">
+                <Outlet/>
+            </div>
+        </>
+
     );
 }
 
