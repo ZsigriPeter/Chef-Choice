f5512dba4155981686b36532597112afcaf2ca96
ZsigriPeter <145133141+ZsigriPeter@users.noreply.github.com>
2024-06-26 09:30:24 +0200

    NavBar works, MainPage not

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+
+
+function NavBar() {
+
+    return (
+        <nav>
+            <div className="nav-bar">
+            <img src="" className="logo-img" alt="logo of the webpage"></img>
+            <ul className="nav-links">
+                <li><a href="/">Weekly menu</a></li>
+                <li><a href="/">Food list</a></li>
+                <li><a href="/">FAQ</a></li>
+                <li><a href="/">Orders</a></li>
+                <li><a href="/">Favorites</a></li>
+                <li><a href="/">User profile</a></li>
+                <li><a href="/">Log In</a></li>
+            </ul>
+        </div>
+        </nav>
+        
+    );
+}
+
+export default NavBar;
