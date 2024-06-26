e0dbc967fb62555467afc7d8540be87f02ba7872
ZsigriPeter <145133141+ZsigriPeter@users.noreply.github.com>
2024-06-26 14:13:25 +0200

    endpoint start

--- backend/src/main/java/com/codecool/backend/controller/DishController.java
+++ backend/src/main/java/com/codecool/backend/controller/DishController.java
@@ -1,4 +1,27 @@
 package com.codecool.backend.controller;
 
+import com.codecool.backend.controller.dto.MainPageDTO;
+import com.codecool.backend.modell.Dish;
+import com.codecool.backend.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+//    @GetMapping("/weekly-menu")
+//    public List<Dish> getDishes() {
+//        return dishService.getAllDishes();
+//    }
+
+    @GetMapping("/main-page")
+    public MainPageDTO getMainPageContent() {
+        return new MainPageDTO("Daily message","picture URL");
+    }
 }
---
+++ backend/src/main/java/com/codecool/backend/controller/dto/MainPageDTO.java
@@ -0,0 +1,4 @@
+package com.codecool.backend.controller.dto;
+
+public record MainPageDTO(String message, String pictureUrl) {
+}
