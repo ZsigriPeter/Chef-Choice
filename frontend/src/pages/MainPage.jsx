c895b0a79a24a71ae477ffeac58b12608fe879b9
ZsigriPeter <145133141+ZsigriPeter@users.noreply.github.com>
2024-06-26 15:02:09 +0200

    Main page gets data from backend

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,13 +1,39 @@
+import {useEffect, useState} from "react";
 
+const fetchMainPage = () => {
+    return fetch("/main-page").then((res) => res.json());
+};
 
 function MainPage() {
 
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchMainPage()
+            .then((result) => {
+                setData(result);
+            })
+    }, []);
+
     return (
         <div className="content">
-            <h2>
-                Welcome to ... !
-            </h2>
-            <img src="" alt="Main Page"/>
+            {
+                data ? (
+                    <>
+                        <h2>
+                            {data.message}
+                        </h2>
+                        <img src={data.pictureUrl} alt="Main Page"/>
+                    </>
+                ) : (
+                    <>
+                        <h2>
+                            Welcome to ... !
+                        </h2>
+                        <img src="" alt="Main Page"/>
+                    </>
+                )
+            }
         </div>
     );
 }
