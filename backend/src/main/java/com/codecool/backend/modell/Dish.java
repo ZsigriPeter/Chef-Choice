a127c142f8993e7ad43b5aaf5155620868ce8784
Levente <145211298+fulopl@users.noreply.github.com>
2024-06-26 11:06:05 +0200

    add MVC structure, add Dish class

---
+++ backend/src/main/java/com/codecool/backend/modell/Dish.java
@@ -0,0 +1,15 @@
+package com.codecool.backend.modell;
+
+public class Dish {
+    private int id;
+    private String name;
+    private String description;
+    private String imageURL;
+    private double price;
+    private Course course;
+    private double averageRating;
+    private double weight;
+    private int stock;
+    private Allergen allergen;
+
+}
