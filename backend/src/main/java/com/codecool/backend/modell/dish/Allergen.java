b71c7569f513bdfc8063a8f434a8e5ff693e775d
Levente <145211298+fulopl@users.noreply.github.com>
2024-06-27 10:21:48 +0200

    add all allergenes

--- backend/src/main/java/com/codecool/backend/modell/dish/Allergen.java
+++ backend/src/main/java/com/codecool/backend/modell/dish/Allergen.java
@@ -4,28 +4,32 @@ public enum Allergen {
     A1(1, "Gluten"),
     A2(2, "Crustaceans"),
     A3(3, "Eggs"),
-    A4(4, "Fish");
+    A4(4, "Fish"),
+    A5(5, "Peanuts"),
+    A6(6, "Soybeans"),
+    A7(7, "Milk"),
+    A8(8, "Tree nuts"),
+    A9(9, "Celery"),
+    A10(10, "Mustard"),
+    A11(11, "Sesame seeds"),
+    A12(12, "Sulphur dioxide and sulphites"),
+    A13(13, "Lupin"),
+    A14(14, "Molluscs")
+    ;
 
-    public int id;
-    public String name;
+    public final int id;
+    public final String name;
 
     Allergen(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-/*    Gluten
-            Crustaceans
-    Eggs
-            Fish
-    Peanuts
-            Soybeans
-    Milk
-    Tree nuts
-    Celery
-            Mustard
-    Sesame seeds
-    Sulphur dioxide and sulphites
-    Lupin
-            Molluscs*/
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
